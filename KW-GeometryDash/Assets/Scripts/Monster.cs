244677693124fa74c30c23af6cc2e0fa310510db
hanheliang45 <hanheliang45@gmail.com>
2024-03-02 15:25:07 -0800

    Monster Follows

--- KW-GeometryDash/Assets/Scripts/Monster.cs
+++ KW-GeometryDash/Assets/Scripts/Monster.cs
@@ -12,9 +12,13 @@ public class Monster : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Vector2 My_position = this.transform.position;
-        Vector2 told_Position = Chracter.instance.GetTransform().position + new Vector3(-6,0);
-        My_position.x = told_Position.x;
+    {  
+        // Vector2 My_position = this.transform.position;
+        // Vector2 told_Position = Chracter.instance.GetTransform().position + new Vector3(-6,0);
+        // My_position.x = told_Position.x;
+        // transform.position = My_position;
+        float position_x = Chracter.instance.GetTransform().position.x - 6;
+        float position_y = this.transform.position.y;
+        transform.position = new Vector3(position_x, position_y, 0);
     }
 }
