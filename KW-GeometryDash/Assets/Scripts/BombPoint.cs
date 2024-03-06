5284854007b62fe924ffebfef0b66b462477065d
hanheliang45 <hanheliang45@gmail.com>
2024-03-05 18:30:26 -0800

    Fire Anim

---
+++ KW-GeometryDash/Assets/Scripts/BombPoint.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject != Chracter.instance.gameObject)
+        {
+            return;    
+        }
+        Monster.instance.Fire();
+    }
+} 
--- KW-GeometryDash/Assets/Scripts/Monster.cs
+++ KW-GeometryDash/Assets/Scripts/Monster.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    public static Monster instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +21,13 @@ public class Monster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {  
-        // Vector2 My_position = this.transform.position;
-        // Vector2 told_Position = Chracter.instance.GetTransform().position + new Vector3(-6,0);
-        // My_position.x = told_Position.x;
-        // transform.position = My_position;
         float position_x = Chracter.instance.GetTransform().position.x - 6;
         float position_y = this.transform.position.y;
         transform.position = new Vector3(position_x, position_y, 0);
+    }
+
+    public void Fire()
+    {
+        Debug.Log("Booooooom");     
     }
 }
