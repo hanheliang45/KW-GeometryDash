b75ca9c30fc9623cbb2e099a1af95d3c772898c6
hanheliang45 <hanheliang45@gmail.com>
2024-03-17 16:23:27 -0800

    Level generating

--- KW-GeometryDash/Assets/Scripts/ButtonManager.cs
+++ KW-GeometryDash/Assets/Scripts/ButtonManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
@@ -8,11 +9,13 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] Transform button;
     [SerializeField] Transform panel;
     [SerializeField] Transform board;
+    [SerializeField] Transform levels;
     [SerializeField] int buttonsPerRow;
     [SerializeField] int panelsPerC;
+    bool DoneMakingButtons;
     int buttonsMade_LF;
     int buttonsMade_T;
-    bool DoneMakingButtons;
+    Transform newPanel = null;
     void Start()
     {
         buttonsMade_LF = 0;
@@ -23,24 +26,31 @@ public class ButtonManager : MonoBehaviour
 
     void Update()
     {
-        Transform newPanel = panel;
+
         if (DoneMakingButtons) 
         {
             return;
         }
+
+        if (buttonsMade_LF == 0) 
+        {
+            newPanel = Instantiate(panel,board);
+        }
+
         if (buttonsMade_T == buttonsPerRow) 
         {
             buttonsMade_T = 0;
             newPanel = Instantiate(panel,board);
         }
-        Instantiate(button,newPanel);    
+
+        Transform newButton = Instantiate(button,newPanel);    
         buttonsMade_LF++;
         buttonsMade_T++;
-        button.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text = buttonsMade_LF.ToString();
+        newButton.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text = buttonsMade_LF.ToString();
         if (buttonsMade_LF == panelsPerC * buttonsPerRow)
         {
-            DoneMakingButtons = true;
-            button.gameObject.SetActive(false);
+            DoneMakingButtons = true;    
+             button.gameObject.SetActive(false);
             panel.gameObject.SetActive(false);
         }
     }
---
+++ KW-GeometryDash/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+    Transform template;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(0);
+        Instantiate(template);
+    }
+
+    public void SetTemplate(Transform template) 
+    {
+        this.template = template;    
+    }
+}
